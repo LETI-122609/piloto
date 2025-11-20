df81f92251388b9309c8d9efec9f4ec8d0b903a7
LETI-122609 <dmsaa4@iscte-iul.pt>
2025-11-20 14:09:38 +0000

    push

---
+++ README.md
@@ -0,0 +1,6 @@
+## Grupo TP01-10
+
+### Membros:
+1. Duarte Sá - Número: 122609
+2. Henrique Lopes - Número: 122634
+3. Martim Teixeira - Número: 122636
--- src/test/java/org/example/piloto/MainPage.java
+++ src/test/java/org/example/piloto/MainPage.java
@@ -1,4 +1,5 @@
 package org.example.piloto;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
