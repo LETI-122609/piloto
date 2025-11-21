10dd80d89ade6f4c08ad1dfa6d0e0a4fd1391338
LETI_122634 <hclse@iscte-iul.pt>
2025-11-21 12:59:34 +0000

    MainPageTeste validada

--- src/test/java/org/example/piloto/MainPage.java
+++ src/test/java/org/example/piloto/MainPage.java
@@ -1,25 +1,39 @@
 package org.example.piloto;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-    @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
-    public WebElement seeDeveloperToolsButton;
+    private final WebDriver driver;
 
-    @FindBy(xpath = "//*[@data-test='suggestion-action']")
-    public WebElement findYourToolsButton;
-
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
-    public WebElement toolsMenu;
-
-    @FindBy(css = "[data-test='site-header-search-action']")
-    public WebElement searchButton;
+    // Expor localizadores para uso com WebDriverWait nos testes
+    public final By seeDeveloperToolsBy = By.xpath("//*[@data-test-marker='Developer Tools']");
+    public final By findYourToolsBy = By.xpath("//*[@data-test='suggestion-action']");
+    public final By toolsMenuBy = By.xpath("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
+    public final By searchButtonBy = By.cssSelector("[data-test='site-header-search-action']");
 
     public MainPage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    // Métodos utilitários simples
+    public WebElement getSearchButton() {
+        return driver.findElement(searchButtonBy);
+    }
+
+    public WebElement getToolsMenu() {
+        return driver.findElement(toolsMenuBy);
+    }
+
+    public WebElement getSeeDeveloperToolsButton() {
+        return driver.findElement(seeDeveloperToolsBy);
+    }
+
+    public WebElement getFindYourToolsButton() {
+        return driver.findElement(findYourToolsBy);
     }
 }
