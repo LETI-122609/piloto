543ebac91ff45f52bc478479182757de2928e7bb
Henrique Lopes <hclse@iscte-iul.pt>
2025-12-04 14:36:27 +0000

    Update DynamicLoadingPage.java
    Voltar a fazer comit da parte dinamica

--- src/test/java/org/example/piloto/DynamicLoadingPage.java
+++ src/test/java/org/example/piloto/DynamicLoadingPage.java
@@ -2,27 +2,20 @@ package org.example.piloto;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoadingPage {
 
-    private final WebDriver driver;
+    private WebDriver driver;
 
-    // URL da página
-    public static final String PAGE_URL = "https://the-internet.herokuapp.com/dynamic_loading/2";
-
-    // Localizadores expostos para o teste usar WebDriverWait
-    public final By startButtonBy = By.cssSelector("#start button");
-    public final By loadingBy = By.id("loading");
-    public final By finishTextBy = By.id("finish");
+    public By startButtonBy = By.cssSelector("#start button");
+    public By loadingBy = By.id("loading");
+    public By finishTextBy = By.cssSelector("#finish h4");
 
     public DynamicLoadingPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
     }
 
-    // Navegação
     public void abrirPagina() {
-        driver.get(PAGE_URL);
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
     }
 }
